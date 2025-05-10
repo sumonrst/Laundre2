d2ff4a72a4007868e473a3cf15cd675cba7ad60e
sumonrst <eng.sumonrejata@gmail.com>
2025-05-11 02:21:16 +0600

    hero slider done
    hero slider done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){
-    $('.banner_area').slick({
+    $('.banner-area').slick({
         // infinite: true,
         // loop: true,
         prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-right"></i></button>',
@@ -26,6 +26,8 @@ $(document).ready(function(){
           ]
         
     });
+
+    
     $('.testimonial').slick({
         autoplay: true,
         slidesToShow:2,
@@ -59,25 +61,6 @@ $(document).ready(function(){
 
   });
 
-
-/* Menu scroll code here start */
-$(window).scroll(function() {
-    /* Menu scroll background color change start */
-    if($(window).scrollTop() > 200) {
-        $('.header_area').addClass('sticky');
-    }else{
-        $('.header_area').removeClass('sticky');
-    }
-    /* Menu scroll background color change end */
-
-    /*  Menu scroll font color change start */ 
-    if($(window).scrollTop() > 200) {
-        $('.header_menu ul li a').addClass('fontcolor');
-    }else{
-        $('.header_menu ul li a').removeClass('fontcolor');
-    }
-    /*  Menu scroll font color change end */ 
-});  
 
 
 
